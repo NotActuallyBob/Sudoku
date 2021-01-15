74496863c70b81925f46cfe7b52be7e73a7f9103
Mikael Virkkunen <44874012+NotActuallyBob@users.noreply.github.com>
2021-01-15 13:33:35 +0200

    2

---
+++ Sudoku/Assets/Scripts/Tile.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Tile : MonoBehaviour
+{
+    int value;
+
+    SpriteRenderer spriteRenderer;
+    [SerializeField]Sprite[] sprites;
+
+    public Tile(int _value) {
+        this.value = _value;
+    }
+
+    public void setValue(int _value) {
+        value = _value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = sprites[value];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
